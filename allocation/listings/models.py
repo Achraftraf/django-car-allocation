0a095e981f97c6c0821cf44b9b708562c3dcbfa9
Achraftraf <achrafzarouki20@gmail.com>
2023-12-03 12:28:53 +0100

    now i add an object in the db using " python manage.py shell "

--- allocation/listings/models.py
+++ allocation/listings/models.py
@@ -10,3 +10,22 @@ class Allocation(models.Model):
 
     class Meta:
         db_table = 'allocation'
+        
+        
+        
+        # add an object 
+        
+        
+#         from listings.models import Allocation  
+
+# # Create an Allocation object
+# allocation_obj = Allocation(
+#     allocation_date='2023-12-03',
+#     allocation_price=150.00,
+#     allocation_nbr_days=5,
+#     client_id=1,
+#     car_id=4
+# )
+
+# # Save the object to the database
+# allocation_obj.save()
