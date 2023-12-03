09574f49fe4fafc29a36b2c84f745858d8b3d54f
Achraftraf <achrafzarouki20@gmail.com>
2023-12-03 12:00:47 +0100

    i add my model  for the class allocation to create my db

--- allocation/listings/models.py
+++ allocation/listings/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Allocation(models.Model):
+    allocation_id = models.AutoField(primary_key=True)
+    allocation_date = models.DateField()
+    allocation_price = models.DecimalField(max_digits=10, decimal_places=2)
+    allocation_nbr_days = models.IntegerField()
+    client_id = models.IntegerField()
+    car_id = models.IntegerField()
+
+    class Meta:
+        db_table = 'allocation'
